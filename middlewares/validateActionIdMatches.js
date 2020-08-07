a77adfe216e96024da40e41dd35200dd98873fd7
haileyhansard <haileyhansard@gmail.com>
2020-08-07 15:34:40 -0700

    Cleaned up validateActionIdMatches

--- middlewares/validateActionIdMatches.js
+++ middlewares/validateActionIdMatches.js
@@ -1,16 +1,7 @@
-const actionModel = require('../data/helpers/actionModel');
-
 module.exports = function validateActionIdMatches(req, res, next) {
     if(req.body.project_id !== req.params.id) {
-        res.status(400).json({ message: 'The project id must match the project id you are tryin to target'})
+        res.status(400).json({ message: 'The project id must match the project id you are trying to target'})
     } else {
         next()
     }
-;}
-
-
-//actionModel.get(req.params.id)
-    //     .then(id => {
-    //         if
-    //     })
-    //     .catch()
+};
